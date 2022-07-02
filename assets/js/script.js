548443d2f9f83e93376a955f69fdb420e18310f9
chang wong <changcwong@gmail.com>
2022-07-01 19:01:21 -0700

    fixed clear button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,34 @@
 // TODO: Declare any global variables we need
-
+let headsFlips = 0
+let tailsFlips = 0
 
 document.addEventListener('DOMContentLoaded', function () {
-    // This is just a sanity check to make sure your JavaScript script is getting loaded
-    // You can remove it once you see it in your browser console in the developer tools
-    console.log('Hi')
+    
+    document.querySelector('#flip').addEventListener('click', () => {
+        let flippedHeads = Math.random() < 0.5;
+        if (flippedHeads) {
+            document.querySelector('#penny-image').src = './assets/images/penny-heads.jpg';
+            document.querySelector('#message').textContent = "You Flipped Heads!";
+            headsFlips++;
+        } else {
+        document.querySelector('#penny-image').src = './assets/images/penny-tails.jpg';
+            document.querySelector('#message').textContent = "You Flipped Tails!";
+            tailsFlips++;
+    }
 
+    let totalFlips = headsFlips + tailsFlips;
+    let percentHeads = 0;
+    let percentTails = 0;
+
+    if (totalFlips > 0) {
+        percentHeads = Math.round((headsFlips / totalFlips) * 100);
+        percentTails = Math.round((tailsFlips / totalFlips) * 100);
+    }
+
+        document.querySelector("#heads").textContent = headsFlips;
+        document.querySelector("#heads-percent").textContent = percentHeads + '%';
+        document.querySelector("#tails").textContent = tailsFlips;
+        document.querySelector("#tails-percent").textContent = percentTails + '%';
     // TODO: Add event listener and handler for flip and clear buttons
 
     // Flip Button Click Handler
@@ -19,9 +42,29 @@ document.addEventListener('DOMContentLoaded', function () {
         // HINT: Make sure not to divide by 0! (if total is 0, percent will be 0 as well)
         // TODO: Update the display of each table cell
 
+    document.querySelector("#clear").addEventListener("click", function () {
+        headsFlips = 0;
+        tailsFlips = 0;
+
+         document.querySelector('#message').textContent = "Let's Get Rolling!"
+         let totalFlips = headsFlips + tailsFlips;
+         let percentHeads = 0;
+         let percentTails = 0;
+
+        if (totalFlips > 0) {
+            percentHeads = Math.round((headsFlips / totalFlips) * 100);
+            percentTails = Math.round((tailsFlips / totalFlips) * 100);
+        }
+    
+        document.querySelector("#heads").textContent = headsFlips;
+        document.querySelector("#heads-percent").textContent = percentHeads + '%';
+        document.querySelector("#tails").textContent = tailsFlips;
+        document.querySelector("#tails-percent").textContent = percentTails + '%';
+    })
 
     // Clear Button Click Handler
         // TODO: Reset global variables to 0
         // TODO: Update the scoreboard (same logic as in flip button click handler)
 
+    })
 })
